183191d717df6fa965821069d76e933487b34ccf
Sourik Bhuiya <sourikbhuiya@gmail.com>
2023-04-29 22:23:09 +0530

    Updates

---
+++ precache-manifest.c8b8a14d37e3d8f573d3f0152ecdfe27.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4a606c71fa6fe5eb7c7",
+    "url": "/static/css/main.0713a117.chunk.css"
+  },
+  {
+    "revision": "e4a606c71fa6fe5eb7c7",
+    "url": "/static/js/main.e4a606c7.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "7b03fc54b8a798d5ea3a",
+    "url": "/static/js/2.7b03fc54.chunk.js"
+  },
+  {
+    "revision": "316baba7ca5f9e773de3",
+    "url": "/static/js/runtime~main.316baba7.js"
+  },
+  {
+    "revision": "897868218d80e56201c2b5a2f88e31f8",
+    "url": "/static/media/Logo.89786821.png"
+  },
+  {
+    "revision": "53bda3ddc2994fffc0671b7868386844",
+    "url": "/static/media/instagram.53bda3dd.png"
+  },
+  {
+    "revision": "566a374814bf2089e3af2607b32f7f92",
+    "url": "/static/media/twitter.566a3748.png"
+  },
+  {
+    "revision": "d752324021f48d1b9011aea5a0baad48",
+    "url": "/static/media/BIKU.d7523240.jpeg"
+  },
+  {
+    "revision": "3eef84b67a119cbcc8ff724956f7df29",
+    "url": "/index.html"
+  }
+];
